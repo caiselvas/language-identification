046513353447b564923b72748f71f26b7d86752b
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-03-01 12:25:51 +0100

    FIX validation MOD smooth to accept totals param
    note: now validation isn't k folds

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 import re
 from nltk.collocations import TrigramCollocationFinder
 import json
-
+import random
 # Function to preprocess texts
 def preprocess_text(text: str) -> str:
 	new_text = ""
@@ -53,23 +53,22 @@ for language in train.keys():
 
 
 trigrams_val, unique_chars_val = {}, {}
-# For 10-folds cross-validation:
-for fold in range(10):
-	trigrams_val[fold], unique_chars_val[fold] = {}, {}
-	for language in train.keys():
-		val_lang = train[language].split("  ")
-		fold_size = len(val_lang)//10
-		train_fold = val_lang[fold*fold_size: (fold+1)*fold_size]
-		train_fold= "  ".join(train_fold)
-		test_fold = val_lang[:fold*fold_size] + val_lang[(fold+1)*fold_size:]
 
-		trigram_finder = TrigramCollocationFinder.from_words(train_fold)
+for language in train.keys():
+	val_lang = train[language].split("  ")
+	fold_size = len(val_lang)//10
+	random.shuffle(val_lang)
+	train_fold = val_lang[fold_size:]
+	train_fold= "  ".join(train_fold)
+	test_fold = val_lang[:fold_size]
 
-		tmp = {key: value for key, value in trigram_finder.ngram_fd.items() if value > 5}
-		tmp = {"".join(map(str, k)): v for k, v in tmp.items()}
-		
-		trigrams_val[fold][language]= (tmp, test_fold)
-		unique_chars_val[fold][language] = len(set("".join("".join(a) for a in tmp.keys())))
+	trigram_finder = TrigramCollocationFinder.from_words(train_fold)
+
+	tmp = {key: value for key, value in trigram_finder.ngram_fd.items() if value > 5}
+	tmp = {"".join(map(str, k)): v for k, v in tmp.items()}
+	
+	trigrams_val[language]= (tmp, test_fold)
+	unique_chars_val[language] = len(set("".join("".join(a) for a in tmp.keys())))
 
 # Convert trigrams to string in order to save the dictionary as a json file
 def dict_trigramtuple_to_string(d: dict) -> dict:
